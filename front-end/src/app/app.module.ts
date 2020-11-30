c1b177e8c8c595c562907bb74731f566507f32eb
guiaraujo0513G <guilherme.oliveira128@fatec.sp.gov.br>
2020-11-30 16:00:49 +0000

    (30/11) Criação Toolbar

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { MainMenuComponent } from './ui/main-menu/main-menu.component';
+import { MainToolbarComponent } from './ui/main-toolbar/main-toolbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainMenuComponent,
+    MainToolbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
